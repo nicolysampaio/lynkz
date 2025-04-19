439f02ddddb7415602ec382343e26efd2493f174
Nicoly Sampaio <nicoly.sampaioluz@gmail.com>
2025-04-19 18:55:11 -0300

    chore: update Button component to make route prop optional and enhance click handling; modify Course page to utilize updated Button functionality

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,22 +5,29 @@ import * as fa from "@fortawesome/free-solid-svg-icons";
 interface ButtonProps {
   label: string;
   className?: string;
-  route: string;
+  route?: string;
+  icon?: fa.IconDefinition;
+  onClick?: () => void;
 }
 
-function Button({ label, className = "", route, ...props }: ButtonProps) {
+function Button({ label, className = "", route, icon = fa.faRightToBracket, onClick, ...props }: ButtonProps) {
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (route) {
+      navigate(route);
+    } else if (onClick) {
+      onClick();
+    }
+  };
 
   return (
     <button
       className={`py-2 px-4 rounded-md flex items-center gap-2 cursor-pointer ${className}`}
+      onClick={handleClick}
       {...props}
-      onClick={() => navigate(route)}
     >
-      <FontAwesomeIcon
-        icon={fa.faRightToBracket}
-        style={{ color: "#ffffff" }}
-      />
+      <FontAwesomeIcon icon={icon} style={{ color: "#ffffff" }} />
       {label}
     </button>
   );
