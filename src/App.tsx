9decbc73f096d6e733002a557be8a46ded35ac40
Nicoly Sampaio <nicoly.sampaioluz@gmail.com>
2025-04-19 19:37:59 -0300

    chore: add DisciplineSelection route to App component and update Course page Button to navigate to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Course from "./pages/Course.tsx";
 import Login from "./pages/Login.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import DisciplineSelection from "./pages/DisciplineSelection.tsx";
 
 function App() {
-    return (
-        <GoogleOAuthProvider clientId="672860559215-km24kctijodjb48k8tnsvhat1em68jj5.apps.googleusercontent.com">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/cursos" element={<Course />} />
-                </Routes>
-            </BrowserRouter>
-        </GoogleOAuthProvider>
-    )
+  return (
+    //TODO: adicionar variáveis de ambiente e atulizar o client id
+    <GoogleOAuthProvider clientId="672860559215-km24kctijodjb48k8tnsvhat1em68jj5.apps.googleusercontent.com">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cursos" element={<Course />} />
+          <Route path="/disciplinas-cursadas" element={<DisciplineSelection />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  );
 }
 
-export default App
+export default App;
