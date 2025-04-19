32af8bbd1f73f4b3d9f6f05ddaa7784a4875ed0f
Nicoly Sampaio <nicoly.sampaioluz@gmail.com>
2025-04-19 20:46:02 -0300

    fix: refactor CardObrigatoria and DisciplineSelection components for improved functionality and readability; add unique keys for mapped elements

--- src/components/Subjects/CardObrigatoria.tsx
+++ src/components/Subjects/CardObrigatoria.tsx
@@ -1,25 +1,29 @@
 interface CardObrigatoriaProps {
-    id: number
-    discipline: string;
-    credits: number | null;
-    selected: boolean;
-    toggleDiscipline: (disciplineId: number | null) => void;
+  id: number;
+  discipline: string;
+  credits: number | null;
+  selected: boolean;
+  toggleDiscipline: (disciplineId: number) => void;
 }
 
-function CardObrigatoria(
-    {
-        id,
-        discipline,
-        credits,
-        selected
-    }: CardObrigatoriaProps
-) {
-    return (
-        <div className={`bg-gray-300 p-4 rounded-md cursor-pointer ${selected && "bg-green-800 text-white"}`} onClick={() => console.log(id)}>
-            <h4 className="font-bold">{discipline}</h4>
-            <p className="text-xs text-gray-500">{credits} créditos</p>
-        </div>
-    )
+function CardObrigatoria({
+  id,
+  discipline,
+  credits,
+  selected = false,
+  toggleDiscipline,
+}: CardObrigatoriaProps) {
+  return (
+    <div
+      className={`bg-gray-300 p-4 rounded-md cursor-pointer ${
+        selected ? "bg-green-800 text-white" : ""
+      }`}
+      onClick={() => toggleDiscipline(id)}
+    >
+      <h4 className="font-bold">{discipline}</h4>
+      <p className="text-xs text-gray-400">{credits} créditos</p>
+    </div>
+  );
 }
 
 export default CardObrigatoria;
--- src/pages/DisciplineSelection.tsx
+++ src/pages/DisciplineSelection.tsx
@@ -1,6 +1,5 @@
-import {useState} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import disciplines from "../../db/disciplines.json";
 import Header from "../components/Header.tsx";
 import Button from "../components/Button.tsx";
@@ -10,114 +9,143 @@ import CardHumanidade from "../components/Subjects/CardHumanidade.tsx";
 import CardOptativa from "../components/Subjects/CardOptativa.tsx";
 
 interface Discipline {
-    id: number;
-    code: string;
-    name: string;
-    credits: number;
-    period: string;
-    quarter: number;
-    courseCategory: string[];
+  id: number;
+  code: string;
+  name: string;
+  credits: number;
+  period: string;
+  quarter: number;
+  courseCategory: string[];
 }
 
 function DisciplineSelection() {
-    const [disciplinesSelected, setDisciplinesSelected] = useState<number[]>([]);
-    const [humanitiesSelected, setHumanitiesSelected] = useState({1: null, 5: null, 6: null});
-    const [openHumanitiesDropdown, setOpenHumanitiesDropdown] = useState({1: false, 5: false, 6: false});
+  const [disciplinesSelected, setDisciplinesSelected] = useState<number[]>([]);
+  const [humanitiesSelected, setHumanitiesSelected] = useState({
+    1: null,
+    5: null,
+    6: null,
+  });
+  const [openHumanitiesDropdown, setOpenHumanitiesDropdown] = useState({
+    1: false,
+    5: false,
+    6: false,
+  });
 
-    const toggleDisciplineSelection = (id: number) =>
-        setDisciplinesSelected
-        ((selected) => (selected.includes(id) ? selected.filter((discipline) => discipline !== id) : [...selected, id]));
-
-    const toggleHumanitiesDropdown = (quarter: number) =>
-        setOpenHumanitiesDropdown((selected) => ({...selected, [quarter]: !selected[quarter]}));
-
-    const handleHumanitiesSelection = (quarter: number, courseId: number | null) =>
-        setHumanitiesSelected((selected) => ({...selected, [quarter]: courseId}));
-
-    const humanitiesCourses = [
-        {id: 1, name: "Bases Epistemológicas da Ciência Moderna"},
-        {id: 2, name: "Ciência, Tecnologia e Sociedade"},
-        {id: 3, name: "Estrutura e Dinâmica Social"},
-        {id: 4, name: "Nenhuma"},
-    ];
-
-    const filteredDisciplines = disciplines.filter(
-        (discipline) => discipline.quarter && discipline.courseCategory?.some((category) => [
-            "BC&T - Bacharelado em Ciência e Tecnologia (OBR)",
-            "BCC - Bacharelado em Ciências da Computação (OBR)",
-            "BCC - Bacharelado em Ciências da Computação (OL)",
-            "Livres"]
-            .includes(category)
-        )
+  const toggleDisciplineSelection = (id: number) =>
+    setDisciplinesSelected((selected) =>
+      selected.includes(id)
+        ? selected.filter((discipline) => discipline !== id)
+        : [...selected, id]
     );
 
-    const groupedDisciplines = filteredDisciplines.reduce<Record<number, Discipline[]>>(
-        (acc, discipline) => {
-            acc[discipline.quarter] = acc[discipline.quarter] || [];
-            acc[discipline.quarter].push(discipline);
-            return acc;
-        }, {}
-    );
+  const toggleHumanitiesDropdown = (quarter: number) =>
+    setOpenHumanitiesDropdown((selected) => ({
+      ...selected,
+      [quarter]: !selected[quarter],
+    }));
 
+  const handleHumanitiesSelection = (
+    quarter: number,
+    courseId: number | null
+  ) =>
+    setHumanitiesSelected((selected) => ({ ...selected, [quarter]: courseId }));
 
-    return (
-        <div className="min-h-screen flex flex-col flex-1 bg-gray-50">
-            <Header/>
+  const humanitiesCourses = [
+    { id: 1, name: "Bases Epistemológicas da Ciência Moderna" },
+    { id: 2, name: "Ciência, Tecnologia e Sociedade" },
+    { id: 3, name: "Estrutura e Dinâmica Social" },
+    { id: 4, name: "Nenhuma" },
+  ];
 
-            <main className="container flex flex-col flex-1 mx-auto px-4 py-8">
-                <div className="bg-white rounded-xl shadow-xs w-full border border-gray-200 p-4">
-                    <h4 className="mb-4 font-semibold text-lg">
-                        Selecione as disciplinas que você já cursou
-                    </h4>
+  const filteredDisciplines = disciplines.filter(
+    (discipline) =>
+      discipline.quarter &&
+      discipline.courseCategory?.some((category) =>
+        [
+          "BC&T - Bacharelado em Ciência e Tecnologia (OBR)",
+          "BCC - Bacharelado em Ciências da Computação (OBR)",
+          "BCC - Bacharelado em Ciências da Computação (OL)",
+          "Livres",
+        ].includes(category)
+      )
+  );
 
-                    <div className="grid grid-cols-1 gap-8 mb-8">
-                        {Object.entries(groupedDisciplines).map(([quarter, disciplines]) => {
-                                const q = parseInt(quarter);
-                                const isHumanitiesQuarter = q === 1 || q === 5 || q === 6;
+  const groupedDisciplines = filteredDisciplines.reduce<
+    Record<number, Discipline[]>
+  >((acc, discipline) => {
+    acc[discipline.quarter] = acc[discipline.quarter] || [];
+    acc[discipline.quarter].push(discipline);
+    return acc;
+  }, {});
 
-                                return (
-                                    <div key={quarter}>
-                                        <h3 className="text-lg font-semibold mb-4">{q}º Quadrimestre</h3>
-                                        <div className="grid grid-cols-4 gap-4">
-                                            {isHumanitiesQuarter && (
-                                                <CardHumanidade
-                                                    quarter={q}
-                                                    humanities={humanitiesCourses}
-                                                    selected={humanitiesSelected[q] || null}
-                                                    selectedQuarter={humanitiesSelected}
-                                                    open={openHumanitiesDropdown[q] || false}
-                                                    toggleDropdown={() => toggleHumanitiesDropdown(q)}
-                                                    handleSelect={(disciplineId) => (handleHumanitiesSelection(q, disciplineId))}
-                                                />
-                                            )}
-                                            {disciplines.map((discipline) => (
-                                                <CardObrigatoria
-                                                    id={discipline.id}
-                                                    discipline={discipline.name}
-                                                    credits={discipline.credits}
-                                                    selected={disciplinesSelected.includes(discipline.id)}
-                                                    toggleDiscipline={() => (toggleDisciplineSelection(discipline.id))}
-                                                />
-                                            ))}
-                                            {q === 11 && <CardOptativa/>}
-                                        </div>
-                                    </div>
-                                );
-                            }
-                        )}
+  return (
+    <div className="min-h-screen flex flex-col flex-1 bg-gray-50">
+      <Header />
+
+      <main className="container flex flex-col flex-1 mx-auto px-4 py-8">
+        <div className="bg-white rounded-xl shadow-xs w-full border border-gray-200 p-4">
+          <h4 className="mb-4 font-semibold text-lg">
+            Selecione as disciplinas que você já cursou
+          </h4>
+
+          <div className="grid grid-cols-1 gap-8 mb-8">
+            {Object.entries(groupedDisciplines).map(
+              ([quarter, disciplines]) => {
+                const q = parseInt(quarter);
+                const isHumanitiesQuarter = q === 1 || q === 5 || q === 6;
+
+                return (
+                  <div key={quarter}>
+                    <h3 className="text-lg font-semibold mb-4">
+                      {q}º Quadrimestre
+                    </h3>
+                    <div className="grid grid-cols-4 gap-4">
+                      {isHumanitiesQuarter && (
+                        <CardHumanidade
+                          key={`humanities-${q}`} // Adicione uma key única
+                          quarter={q}
+                          humanities={humanitiesCourses}
+                          selected={humanitiesSelected[q] || null}
+                          selectedQuarter={humanitiesSelected}
+                          open={openHumanitiesDropdown[q] || false}
+                          toggleDropdown={() => toggleHumanitiesDropdown(q)}
+                          handleSelect={(disciplineId) =>
+                            handleHumanitiesSelection(q, disciplineId)
+                          }
+                        />
+                      )}
+                      {disciplines.map((discipline) => (
+                        <CardObrigatoria
+                          key={discipline.id} // Adicione a key aqui
+                          id={discipline.id}
+                          discipline={discipline.name}
+                          credits={discipline.credits}
+                          selected={disciplinesSelected.includes(discipline.id)}
+                          toggleDiscipline={toggleDisciplineSelection}
+                        />
+                      ))}
+                      {q === 11 && <CardOptativa key="optativa" />}
                     </div>
+                  </div>
+                );
+              }
+            )}
+          </div>
 
-                    <div className="w-full flex justify-center">
-                        <Button label="Confirmar seleção" className="bg-green-800 text-white text-sm">
-                            <FontAwesomeIcon icon={faCheck}/>
-                        </Button>
-                    </div>
-                </div>
-            </main>
-
-            <Footer/>
+          <div className="w-full flex justify-center">
+            <Button
+              label="Confirmar seleção"
+              className="bg-green-800 text-white text-sm"
+              onClick={() => console.log(disciplinesSelected)}
+              icon={faCheck}
+            />
+          </div>
         </div>
-    )
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default DisciplineSelection;
